01baa036bb3a69990bf83b699967418ed1327696
Seong Kyu Sohn <nullterra180@gmail.com>
2021-02-16 11:31:38 +0900

    feat: add excel file download button

--- stories/Button.vue
+++ stories/Button.vue
@@ -1,54 +1,69 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+  <button type="button" :class="classes" @click="onClick" :style="style">
+    {{ label }}
+  </button>
 </template>
 
 <script>
-import './button.css';
+import "./button.css";
+import {
+  writeDataToFile,
+  writeSheetData,
+  download,
+  createWorkbook
+} from "./ExcelService";
 
 export default {
-  name: 'my-button',
+  name: "my-button",
 
   props: {
     label: {
       type: String,
-      required: true,
+      default: "Button"
     },
     primary: {
       type: Boolean,
-      default: false,
+      default: false
     },
     size: {
       type: String,
-      default: 'medium',
-      validator: function (value) {
-        return ['small', 'medium', 'large'].indexOf(value) !== -1;
-      },
+      default: "medium",
+      validator: function(value) {
+        return ["small", "medium", "large"].indexOf(value) !== -1;
+      }
     },
     backgroundColor: {
-      type: String,
+      type: String
     },
+    downloadable: {
+      type: Boolean,
+      default: false
+    }
   },
 
   computed: {
     classes() {
       return {
-        'storybook-button': true,
-        'storybook-button--primary': this.primary,
-        'storybook-button--secondary': !this.primary,
-        [`storybook-button--${this.size}`]: true,
+        "storybook-button": true,
+        "storybook-button--primary": this.primary,
+        "storybook-button--secondary": !this.primary,
+        [`storybook-button--${this.size}`]: true
       };
     },
     style() {
       return {
-        backgroundColor: this.backgroundColor,
+        backgroundColor: this.backgroundColor
       };
-    },
+    }
   },
 
   methods: {
     onClick() {
-      this.$emit('onClick');
-    },
-  },
+      this.$emit("onClick");
+      if (!!this.downloadable) {
+        writeDataToFile("ExcelFile", "ExcelSheet1", [], "month");
+      }
+    }
+  }
 };
 </script>
