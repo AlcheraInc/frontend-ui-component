5a1f98dda148e658def33b9bfa5c5fd034ccc19c
Seong Kyu Sohn <nullterra180@gmail.com>
2021-04-07 16:16:18 +0900

    feat: add Tabs story, component

--- base-components/BaseTabs.vue
+++ base-components/BaseTabs.vue
@@ -1,33 +1,53 @@
 <template>
-  <v-tabs v-model="tabModel" :color="backgroundColor" dark show-arrows @change="onChangeTabs($event)">
+  <v-tabs
+    v-model="tabModel"
+    :background-color="backgroundColor"
+    :color="selectedTabTextColor"
+    dark
+    show-arrows
+    @change="onChangeTabs($event)"
+  >
     <v-tabs-slider :color="sliderColor" />
-    <v-tab v-for="(tab, tIndex) in items" :key="tab + tIndex.toString()">{{ tab }}</v-tab>
+    <v-tab v-for="(tab, tIndex) in items" :key="tab + tIndex.toString()">{{
+      tab
+    }}</v-tab>
   </v-tabs>
 </template>
 
 <script>
 export default {
-  name: 'BaseTabs',
+  name: "BaseTabs",
   props: {
+    backgroundColor: {
+      type: String,
+      required: false,
+      default: "primary"
+    },
     items: {
       type: Array,
-      required: true,
+      required: true
+    },
+    selectedTabTextColor: {
+      type: String,
+      required: false,
+      default: "white"
     },
     sliderColor: {
       type: String,
       required: false,
-      default: 'white',
-    },
+      default: "white"
+    }
   },
   data: () => {
     return {
-      tabModel: '',
-    }
+      tabModel: ""
+    };
   },
   methods: {
     onChangeTabs(evt) {
-      console.debug('BaseTabs#onChangeTabs', evt)
-    },
-  },
-}
+      console.debug("BaseTabs#onChangeTabs", evt);
+      this.$emit("change-tabs", evt);
+    }
+  }
+};
 </script>
