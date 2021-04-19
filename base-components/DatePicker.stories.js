c6a92c382216930f470b285ccdbffaa28a36701e
Seong Kyu Sohn <nullterra180@gmail.com>
2021-04-19 10:26:58 +0900

    feat: add base date picker composite component

---
+++ base-components/DatePicker.stories.js
@@ -0,0 +1,57 @@
+import BaseDatePicker from "./BaseDatePicker.vue";
+import Vue from "vue";
+
+export default {
+  title: "basics/DatePicker",
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "composite component comprised of menu, text field, and date picker"
+      }
+    }
+  },
+  component: BaseDatePicker,
+  argTypes: {
+    color: {
+      description: "color of date picker component",
+      control: {
+        type: "color"
+      }
+    },
+    disabled: {
+      description: "prevents user interaction with the text field component",
+      control: {
+        type: "boolean"
+      }
+    },
+    disabledDatePicker: {
+      description: "prevents user interaction with the date picker component",
+      control: {
+        type: "boolean"
+      }
+    },
+    locale: {
+      description: "changes date picker to match given locale value",
+      control: {
+        type: "text"
+      }
+    },
+    type: {
+      description: "changes date picker to month picker",
+      control: {
+        type: "select",
+        options: ["date", "month"]
+      }
+    }
+  }
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { BaseDatePicker },
+  template: '<v-app><BaseDatePicker v-bind="$props" /></v-app>'
+});
+
+export const Example = Template.bind({});
+Example.args = {};
