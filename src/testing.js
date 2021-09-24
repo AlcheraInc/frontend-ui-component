d14f414b98ac42e4024808af4978bbdbf1fe2079
SungHyun (Johnny) Tae <sh.tae@alcherainc.com>
2021-09-24 10:45:25 +0900

    chore: test deploy & npm publish trigger

--- src/testing.js
+++ src/testing.js
@@ -1,6 +1,2 @@
 //testing actions trigger
 //testing actions trigger + npm release trigger
-//testing actions trigger + npm release trigger2
-//testing actions trigger + npm release trigger3
-//testing actions trigger only storybook
-//testing actions trigger only storybook2
